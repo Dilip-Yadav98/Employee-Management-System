d006c3e74374eb7cca43767f56535fe86e17e4fb
Dilip <dilip.zz7523@gmail.com>
2024-10-12 19:53:38 +0530

    Add files via upload

---
+++ Database.sql
@@ -0,0 +1,6 @@
+/*
+-- Query: select*from empdata
+LIMIT 0, 5000
+-- Date: 2024-10-01 12:20
+*/
+INSERT INTO `` (`Id`,`Name`,`Email_id`,`Phoneno`,`Address`,`Post`,`Salary`) VALUES (1,'Dilip','dilip.zz7523@gmail.com',7678504748,'Noida','Frontend Developer',80000);
